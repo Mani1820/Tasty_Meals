308e899d7f8a494b80ffe356d7b4b58b18f1b3f8
Mani <manibadboy66@gmail.com>
2025-01-04 08:31:30 +0530

    #

--- lib/Screens/meal_detail_screen.dart
+++ lib/Screens/meal_detail_screen.dart
@@ -12,9 +12,62 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image(
-        image: NetworkImage(meal.imageUrl),
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 300,
+              ),
+            ),
+            Text(
+              'ingredients',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            for (final ingredient in meal.ingredients)
+              Text(
+                ingredient,
+                textAlign: TextAlign.left,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSecondaryContainer),
+              ),
+            const SizedBox(
+              height: 28,
+            ),
+            Text(
+              'steps',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.primary),
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            for (final step in meal.steps)
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 9),
+                child: Text(
+                  step,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color:
+                          Theme.of(context).colorScheme.onSecondaryContainer),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
